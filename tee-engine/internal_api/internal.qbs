Open-TEE/tee-engine

Inilization of storage api header file and implemented Generic Object functions, Transient Object functions, Persistant object functions, Data stream functions and Enumeraton functions.
Change-Id: I1b5fa72f4cbe67a3843171535d36890eb9edc410
Signed-off-by: Tanel Dettenborn <dettenbo@cs.helsinki.fi>

internal_api/internal.qbs
@@ -9,8 +9,12 @@ DynamicLibrary {
         cpp.includePaths: ["../include", "."]
     }
 
+    cpp.dynamicLibraries: ["ssl", "crypto"]
+
     files: ["../include/tee_internal_api.h", "data_types.h", "tee_ta_interface.h",
-        "tee_memory.h", "tee_memory.c", "time_api.h", "time_api.c"
+        "tee_memory.h", "tee_memory.c", "time_api.h", "time_api.c", "storage_data_key_api.h",
+        "storage_data_key_api.c", "storage_key_apis_external_funcs.c", "tee_panic.h", "tee_panic.c",
+        "tee_storage_common.h", "storage_key_apis_external_funcs.h"
     ]
 }
 

Open-TEE/Open-TEE

tee-engine/internal_api/internal.qbs
@@ -9,8 +9,12 @@ DynamicLibrary {
         cpp.includePaths: ["../include", "."]
     }
 
+    cpp.dynamicLibraries: ["ssl", "crypto"]
+
     files: ["../include/tee_internal_api.h", "data_types.h", "tee_ta_interface.h",
-        "tee_memory.h", "tee_memory.c", "time_api.h", "time_api.c"
+        "tee_memory.h", "tee_memory.c", "time_api.h", "time_api.c", "tee_storage_api.h",
+        "tee_storage_api.c", "storage_key_apis_external_funcs.c", "tee_panic.h", "tee_panic.c",
+        "tee_storage_common.h", "storage_key_apis_external_funcs.h"
     ]
 }
 