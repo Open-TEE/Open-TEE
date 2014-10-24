Open-TEE/tee-engine

TA process mainloop
Change-Id: I5f181dabcb49d26c6c8be35d2d57d2b9b683176a
Signed-off-by: TanelDettenborn <tanel.dettenborn@intel.com>

launcher/launcher.qbs
@@ -30,6 +30,8 @@ DynamicLibrary {
         "dynamic_loader.c",
         "launcher_mainloop.c",
         "ta_extern_resources.h",
+        "ta_internal_api_thread.h",
+        "ta_internal_api_thread.c",
         "ta_process.h",
         "ta_process.c",
     ]

launcher/ta_process.c
@@ -14,14 +14,19 @@
 ** limitations under the License.                                           **
 *****************************************************************************/
 
+#include <errno.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/eventfd.h>
+#include <sys/prctl.h>
 
 #include "com_protocol.h"
 #include "core_extern_resources.h"
 #include "dynamic_loader.h"
+#include "epoll_wrapper.h"
 #include "ta_extern_resources.h"
+#include "ta_internal_api_thread.h"
 #include "ta_process.h"
 #include "tee_logging.h"
 
@@ -42,17 +47,34 @@ struct ta_task tasks_todo;
 /* These are for tasks that are complete and are being returned to the caller */
 struct ta_task tasks_done;
 
+/* Maximum epoll events */
+#define MAX_CURR_EVENTS 5
+
 int ta_process_loop(int man_sockfd, struct com_msg_open_session *open_msg)
 {
-	TEEC_Result ret;
-	struct ta_interface *interface;
-	man_sockfd = man_sockfd;
-	open_msg = open_msg;
+	int ret;
+	pthread_t ta_logic_thread;
+	pthread_attr_t attr;
+	struct epoll_event cur_events[MAX_CURR_EVENTS];
+	int event_count, i;
+	char proc_name[MAX_PR_NAME]; /* For now */
+	sigset_t sig_empty_set;
+
+	/* Set new ta process name */
+	strncpy(proc_name, __FILE__, MAX_PR_NAME);
+	prctl(PR_SET_NAME, (unsigned long)proc_name);
+	strncpy(argv0, proc_name, argv0_len);
 
 	/* Load TA to this process */
 	ret = load_ta(open_msg->ta_so_name, &interface);
 	if (ret != TEE_SUCCESS || interface == NULL) {
 		OT_LOG(LOG_ERR, "Failed to load the TA");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Note: All signal are blocked. Prepare allow set when we can accept signals */
+	if (sigemptyset(&sig_empty_set)) {
+		OT_LOG(LOG_ERR, "Sigempty set failed: %s", strerror(errno))
 		exit(EXIT_FAILURE);
 	}
 
@@ -69,6 +91,92 @@ int ta_process_loop(int man_sockfd, struct com_msg_open_session *open_msg)
 	/* Initializations of TODO and DONE queues*/
 	INIT_LIST(&tasks_todo.list);
 	INIT_LIST(&tasks_done.list);
+
+	/* Init epoll and register FD/data */
+	if (init_epoll())
+		exit(EXIT_FAILURE);
+
+	/* listen to inbound connections from the manager */
+	if (epoll_reg_fd(man_sockfd, EPOLLIN))
+		exit(EXIT_FAILURE);
+
+	/* listen for communications from the TA thread process */
+	if (epoll_reg_fd(event_fd, EPOLLIN))
+		exit(EXIT_FAILURE);
+
+	/* Signal handling */
+	if (epoll_reg_fd(self_pipe_fd, EPOLLIN))
+		exit(EXIT_FAILURE);
+
+	/* Init worker thread */
+	ret = pthread_attr_init(&attr);
+	if (ret) {
+		OT_LOG(LOG_ERR, "Failed to create attr for thread: %s", strerror(errno))
+		exit(EXIT_FAILURE);
+	}
+
+	/* TODO: Should we reserver space for thread stack? */
+
+	ret = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	if (ret) {
+		OT_LOG(LOG_ERR, "Failed set DETACHED: %s", strerror(errno))
+		exit(EXIT_FAILURE);
+	}
+
+	/* Known error: CA can not determ if TA is launched or not, because framework is calling
+	 * create entry point and open session function. Those functions return values is mapped
+	 * into one return value. */
+	if (interface->create() != TEE_SUCCESS) {
+		OT_LOG(LOG_ERR, "TA create entry point failed");
+		exit(EXIT_SUCCESS);
+	}
+
+	/* Launch worker thread and pass open session message as a parameter */
+	ret = pthread_create(&ta_logic_thread, &attr, ta_internal_api_thread, open_msg);
+	if (ret) {
+		OT_LOG(LOG_ERR, "Failed launch thread: %s", strerror(errno))
+		interface->destroy();
+		exit(EXIT_FAILURE);
+	}
+
+	pthread_attr_destroy(&attr); /* Not needed any more */
+
+	/* Allow signal delivery */
+	if (pthread_sigmask(SIG_SETMASK, &sig_empty_set, NULL)) {
+		OT_LOG(LOG_ERR, "failed to allow signals: %s", strerror(errno))
+		exit(EXIT_FAILURE);
+	}
+
+	/* Enter into the main part of this io_thread */
+	for (;;) {
+		event_count = wrap_epoll_wait(cur_events, MAX_CURR_EVENTS);
+		if (event_count == -1) {
+			if (errno == EINTR) {
+
+				continue;
+			}
+
+			/* Log error and hope the error clears itself */
+			OT_LOG(LOG_ERR, "Failed return from epoll_wait");
+			continue;
+		}
+
+		for (i = 0; i < event_count; i++) {
+
+			if (cur_events[i].data.fd == man_sockfd) {
+
+
+			} else if (cur_events[i].data.fd == event_fd) {
+
+
+			} else if (cur_events[i].data.fd == self_pipe_fd) {
+
+
+			} else {
+				OT_LOG(LOG_ERR, "unknown event source");
+			}
+		}
+	}
 
 	/* Should never reach here */
 	exit(EXIT_FAILURE);

Open-TEE/Open-TEE

tee-engine/launcher/launcher.qbs
@@ -30,6 +30,8 @@ DynamicLibrary {
         "dynamic_loader.c",
         "launcher_mainloop.c",
         "ta_extern_resources.h",
+        "ta_internal_thread.h",
+        "ta_internal_thread.c",
         "ta_process.h",
         "ta_process.c",
     ]

tee-engine/launcher/ta_process.c
@@ -14,14 +14,19 @@
 ** limitations under the License.                                           **
 *****************************************************************************/
 
+#include <errno.h>
 #include <pthread.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/eventfd.h>
+#include <sys/prctl.h>
 
 #include "com_protocol.h"
 #include "core_extern_resources.h"
 #include "dynamic_loader.h"
+#include "epoll_wrapper.h"
 #include "ta_extern_resources.h"
+#include "ta_internal_thread.h"
 #include "ta_process.h"
 #include "tee_logging.h"
 
@@ -42,17 +47,34 @@ struct ta_task tasks_todo;
 /* These are for tasks that are complete and are being returned to the caller */
 struct ta_task tasks_done;
 
+/* Maximum epoll events */
+#define MAX_CURR_EVENTS 5
+
 int ta_process_loop(int man_sockfd, struct com_msg_open_session *open_msg)
 {
-	TEEC_Result ret;
-	struct ta_interface *interface;
-	man_sockfd = man_sockfd;
-	open_msg = open_msg;
+	int ret;
+	pthread_t ta_logic_thread;
+	pthread_attr_t attr;
+	struct epoll_event cur_events[MAX_CURR_EVENTS];
+	int event_count, i;
+	char proc_name[MAX_PR_NAME]; /* For now */
+	sigset_t sig_empty_set;
+
+	/* Set new ta process name */
+	strncpy(proc_name, open_msg->ta_so_name, MAX_PR_NAME);
+	prctl(PR_SET_NAME, (unsigned long)proc_name);
+	strncpy(argv0, proc_name, argv0_len);
 
 	/* Load TA to this process */
 	ret = load_ta(open_msg->ta_so_name, &interface);
 	if (ret != TEE_SUCCESS || interface == NULL) {
 		OT_LOG(LOG_ERR, "Failed to load the TA");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Note: All signal are blocked. Prepare allow set when we can accept signals */
+	if (sigemptyset(&sig_empty_set)) {
+		OT_LOG(LOG_ERR, "Sigempty set failed: %s", strerror(errno))
 		exit(EXIT_FAILURE);
 	}
 
@@ -69,6 +91,92 @@ int ta_process_loop(int man_sockfd, struct com_msg_open_session *open_msg)
 	/* Initializations of TODO and DONE queues*/
 	INIT_LIST(&tasks_todo.list);
 	INIT_LIST(&tasks_done.list);
+
+	/* Init epoll and register FD/data */
+	if (init_epoll())
+		exit(EXIT_FAILURE);
+
+	/* listen to inbound connections from the manager */
+	if (epoll_reg_fd(man_sockfd, EPOLLIN))
+		exit(EXIT_FAILURE);
+
+	/* listen for communications from the TA thread process */
+	if (epoll_reg_fd(event_fd, EPOLLIN))
+		exit(EXIT_FAILURE);
+
+	/* Signal handling */
+	if (epoll_reg_fd(self_pipe_fd, EPOLLIN))
+		exit(EXIT_FAILURE);
+
+	/* Init worker thread */
+	ret = pthread_attr_init(&attr);
+	if (ret) {
+		OT_LOG(LOG_ERR, "Failed to create attr for thread: %s", strerror(errno))
+		exit(EXIT_FAILURE);
+	}
+
+	/* TODO: Should we reserver space for thread stack? */
+
+	ret = pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	if (ret) {
+		OT_LOG(LOG_ERR, "Failed set DETACHED: %s", strerror(errno))
+		exit(EXIT_FAILURE);
+	}
+
+	/* Known error: CA can not determ if TA is launched or not, because framework is calling
+	 * create entry point and open session function. Those functions return values is mapped
+	 * into one return value. */
+	if (interface->create() != TEE_SUCCESS) {
+		OT_LOG(LOG_ERR, "TA create entry point failed");
+		exit(EXIT_SUCCESS);
+	}
+
+	/* Launch worker thread and pass open session message as a parameter */
+	ret = pthread_create(&ta_logic_thread, &attr, ta_internal_thread, open_msg);
+	if (ret) {
+		OT_LOG(LOG_ERR, "Failed launch thread: %s", strerror(errno))
+		interface->destroy();
+		exit(EXIT_FAILURE);
+	}
+
+	pthread_attr_destroy(&attr); /* Not needed any more */
+
+	/* Allow signal delivery */
+	if (pthread_sigmask(SIG_SETMASK, &sig_empty_set, NULL)) {
+		OT_LOG(LOG_ERR, "failed to allow signals: %s", strerror(errno))
+		exit(EXIT_FAILURE);
+	}
+
+	/* Enter into the main part of this io_thread */
+	for (;;) {
+		event_count = wrap_epoll_wait(cur_events, MAX_CURR_EVENTS);
+		if (event_count == -1) {
+			if (errno == EINTR) {
+
+				continue;
+			}
+
+			/* Log error and hope the error clears itself */
+			OT_LOG(LOG_ERR, "Failed return from epoll_wait");
+			continue;
+		}
+
+		for (i = 0; i < event_count; i++) {
+
+			if (cur_events[i].data.fd == man_sockfd) {
+
+
+			} else if (cur_events[i].data.fd == event_fd) {
+
+
+			} else if (cur_events[i].data.fd == self_pipe_fd) {
+
+
+			} else {
+				OT_LOG(LOG_ERR, "unknown event source");
+			}
+		}
+	}
 
 	/* Should never reach here */
 	exit(EXIT_FAILURE);