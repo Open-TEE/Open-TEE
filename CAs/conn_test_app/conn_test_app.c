619791e29e8e3392059b3fa8ca1df0d4815352b7
TanelDettenborn <tanel.dettenborn@intel.com>
2014-11-03 11:54:40 +0200

    Conn test app calling all five entry point funciton
    Change-Id: I10c1cb7eaed5d0a5f2fbc98077be39f639ac1fab
    Signed-off-by: TanelDettenborn <tanel.dettenborn@intel.com>

--- CAs/conn_test_app/conn_test_app.c
+++ CAs/conn_test_app/conn_test_app.c
@@ -22,23 +22,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static const TEEC_UUID uuid =
+{
+	0x3E93632E, 0xA710, 0x469E,
+	{ 0xAC, 0xC8, 0x5E, 0xDF, 0x8C, 0x85, 0x90, 0xE1 }
+};
+
 int main()
 {
 	TEEC_Context context;
+	TEEC_Session session;
 	TEEC_Result ret;
+	uint32_t return_origin;
+	uint32_t connection_method = TEEC_LOGIN_PUBLIC;
+
 
 	printf("START: conn test app\n");
 
+
+	/* Initialize context */
+	printf("Initializing context: ");
 	ret = TEEC_InitializeContext(NULL, &context);
 	if (ret != TEEC_SUCCESS) {
 		printf("TEEC_InitializeContext failed: 0x%x\n", ret);
-		goto err;
+		goto end_1;
+	} else {
+		printf("initiliazed\n");
 	}
 
-	printf("END: conn test app\n");
-	exit(EXIT_SUCCESS);
 
-err:
+
+	/* Open session */
+	printf("Openning session: ");
+	ret = TEEC_OpenSession(&context, &session, &uuid,
+			       connection_method, NULL, NULL, &return_origin);
+	if (ret != TEEC_SUCCESS) {
+		printf("TEEC_OpenSession failed: 0x%x\n", ret);
+		goto end_2;
+	} else {
+		printf("opened\n");
+	}
+
+
+
+	/* Invoke command */
+	printf("Invoking command: ");
+	ret = TEEC_InvokeCommand(&session, 0, NULL, &return_origin);
+	if (ret != TEEC_SUCCESS) {
+		printf("TEEC_InvokeCommand failed: 0x%x\n", ret);
+		goto end_3;
+	} else {
+		printf("invoked\n");
+	}
+
+
+
+	/* Cleanup used connection/resources */
+
+end_3:
+	printf("Closing session: ");
+	TEEC_CloseSession(&session);
+	printf("Closed\n");
+
+
+end_2:
+	printf("Finalizing ctx: ");
+	TEEC_FinalizeContext(&context);
+	printf("Finalized\n");
+
+end_1:
 	printf("END: conn test app\n");
 	exit(EXIT_FAILURE);
 }
