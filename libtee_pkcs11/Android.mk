884ebd85a26eeb6a64641ba5b06a08a864ab10cd
uberspot <onexemailx@gmail.com>
2015-05-15 13:32:36 +0300

    make android.mk link against libtee if libdx_cc7 is not present
    Change-Id: I444a7ce777a5e35ae36bb4777de40bbdc21f5f2c

--- libtee_pkcs11/Android.mk
+++ libtee_pkcs11/Android.mk
@@ -3,6 +3,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 ifeq ($(BUILD_WITH_SECURITY_FRAMEWORK), chaabi_token)
+local_shared_libraries := libdx_cc7
+local_c_flags := -DDX_CC_HOST -DDX_CC54_SUPPORTED
+else
+local_shared_libraries := libtee
+local_c_flags :=
+endif #build for chaabi token
 
 local_src_files :=                          \
                 src/hal_gp.c                \
@@ -18,9 +24,6 @@ local_c_includes := \
 
 local_export_c_include_dirs := $(LOCAL_PATH)/include
 
-local_c_flags := -DDX_CC_HOST -DDX_CC54_SUPPORTED
-
-
 #################################################
 # Target dynamic library
 
@@ -29,7 +32,7 @@ LOCAL_SRC_FILES := $(local_src_files)
 LOCAL_C_INCLUDES += $(local_c_includes)
 LOCAL_CFLAGS += $(local_c_flags)
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(local_export_c_include_dirs)
-LOCAL_SHARED_LIBRARIES += libc libdx_cc7
+LOCAL_SHARED_LIBRARIES += libc $(local_shared_libraries)
 LOCAL_MODULE := libtee_pkcs11
 LOCAL_MODULE_TAGS := optional
 LOCAL_COPY_HEADERS_TO := libtee_pkcs11
@@ -49,7 +52,7 @@ LOCAL_C_INCLUDES += $(local_c_includes)
 LOCAL_CFLAGS += $(local_c_flags)
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(local_export_c_include_dirs)
 LOCAL_STATIC_LIBRARIES += libc
-LOCAL_SHARED_LIBRARIES += libdx_cc7
+LOCAL_SHARED_LIBRARIES += $(local_shared_libraries)
 LOCAL_MODULE := libtee_pkcs11_static
 LOCAL_MODULE_TAGS := optional
 LOCAL_COPY_HEADERS_TO := libtee_pkcs11_static
@@ -60,4 +63,3 @@ LOCAL_COPY_HEADERS += \
                include/pkcs11t.h
 include $(BUILD_STATIC_LIBRARY)
 
-endif #build for chaabi token
