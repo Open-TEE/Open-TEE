0d382fde36164c25b6ee5f3f223ca08e79acd6b5
Thomas Nyman <thomas.nyman@aalto.fi>
2015-10-06 18:09:48 +0300

    Fix missing include in conn_test_app Android.mk
    Change-Id: I70223ebb9e2d036ae125a1f4668877bfa7a0e803
    Signed-off-by: Thomas Nyman <thomas.nyman@aalto.fi>

--- CAs/conn_test_app/Android.mk
+++ CAs/conn_test_app/Android.mk
@@ -7,7 +7,8 @@ LOCAL_SRC_FILES	    :=  conn_test_app.c
 
 LOCAL_C_FLAGS       :=  -rdynamic -DANDROID
 
-LOCAL_C_INCLUDES    := $(LOCAL_PATH)
+LOCAL_C_INCLUDES    := $(LOCAL_PATH) \
+                       $(LOCAL_PATH)/../../libtee/include
 
 
 LOCAL_SHARED_LIBRARIES := libdl libtee
