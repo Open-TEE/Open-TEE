0cb4a46c7e258e6bb1ecba1c2f2ef515bfb85570
Mika Tammi <mikatammi@gmail.com>
2026-01-27 19:08:47 +0200

    squash! fixup! WIP: Trusted UI example Trusted Application
    Copyright fix
    
    Signed-off-by: Mika Tammi <mikatammi@gmail.com>

--- TAs/trusted_ui_example_ta/trusted_ui_example_ta.c
+++ TAs/trusted_ui_example_ta/trusted_ui_example_ta.c
@@ -1,5 +1,5 @@
 /*****************************************************************************
-** Copyright (C) 2014 Mika Tammi                                            **
+** Copyright (C) 2014-2026 Mika Tammi                                       **
 **                                                                          **
 ** Licensed under the Apache License, Version 2.0 (the "License");          **
 ** you may not use this file except in compliance with the License.         **
